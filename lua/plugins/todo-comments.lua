054a6dd117ab6ce13e694a46f48a298519f816f8
paul-morenkov <paulmorenkov@gmail.com>
2025-03-23 12:09:32 -0700

    Temporary fix for todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,10 +1,40 @@
-if not vim.g.vscode then
-  return {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
-    config = true,
-  }
-else
-  return {}
-end
+return {
+	"folke/todo-comments.nvim",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {},
+	-- event = {
+	--   "BufReadPre",
+	--   "BufNewFile",
+	-- },
+	keys = {
+		{
+			"<leader>st",
+			function()
+				require("snacks").picker.todo_comments()
+			end,
+			{ desc = "Todo" },
+		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Next Todo",
+		},
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next Todo",
+		},
+	},
+	-- config = function(opts)
+	-- 	local todo = require("todo-commments").setup(opts)
+	-- 	vim.keymap.set("n", "]t", function()
+	-- 		todo.jump_next()
+	-- 	end, { desc = "Next Todo" })
+	-- end,
+	-- TODO: test
+}
