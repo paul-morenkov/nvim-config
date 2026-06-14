173de82f04ffb3be9ddd086f56cbf881fdd0d23e
Paul Morenkov <paulmorenkov@gmail.com>
2026-06-13 21:20:25 -0700

    Add custom mini.align options

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,6 @@
 vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 
 require("mini.ai").setup()
-require("mini.align").setup()
 require("mini.comment").setup()
 -- require("mini.completion").setup() -- TODO: Completion will be handled by something else
 require("mini.move").setup()
@@ -9,3 +8,35 @@ require("mini.pairs").setup()
 require("mini.statusline").setup()
 require("mini.tabline").setup()
 -- require("mini.surround").setup() -- Using nvim-surround instead (better features IMO)
+
+require("mini.align").setup({
+	modifiers = {
+		-- Customize justify to take list of justification patterns
+		j = function(_, opts)
+			local char_to_side = {
+				l = "left",
+				c = "center",
+				r = "right",
+				n = "none",
+			}
+
+			local input = vim.fn.input("Justify (l/c/r/n): ")
+			if input == "" then
+				return
+			end
+
+			local sides = {}
+			for i = 1, #input do
+				local side = char_to_side[input:sub(i, i)]
+				if side then
+					table.insert(sides, side)
+				end
+			end
+
+			if #sides == 0 then
+				return
+			end
+			opts.justify_side = #sides == 1 and sides[1] or sides
+		end,
+	},
+})
